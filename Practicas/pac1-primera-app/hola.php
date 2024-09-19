0a347eae1b26001066688ec19ff1334ee6a6a4af
BrianCamunez <124356373+BrianCamunez@users.noreply.github.com>
2024-09-19 15:24:33 +0000

    pt1

--- Practicas/pac1-primera-app/hola.php
+++ Practicas/pac1-primera-app/hola.php
@@ -1,10 +1,14 @@
 <html>
     <head>
         <title>Practica 1</title>
-        
+        <img src="logo-fpllefia.jfif" alt="FotoLogo">
         <h1>Módulo 7 - Práctica 1. Mi primera aplicación en PHP</h1>
     </head>
     <body>
-        
+        <div>
+            <div class="izquierda">
+                <img src="imagen2.png" alt="">
+            </div>
+        </div>
     </body>
 </html>
