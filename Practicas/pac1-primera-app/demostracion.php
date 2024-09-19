325d222cfc3684d1f133feb4063b94ef0d41c202
BrianCamunez <124356373+BrianCamunez@users.noreply.github.com>
2024-09-19 16:27:26 +0000

    v3

---
+++ Practicas/pac1-primera-app/demostracion.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Lo he sacado del manual de php: <a href="https://www.php.net/manual/es/function.date.php"></a></p>
+    <img src="demostracion.php" alt="Foto demostracion">
+
+</body>
+</html>
