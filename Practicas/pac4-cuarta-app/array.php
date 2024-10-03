b77e49648fce65ead37267b08b29d545152c894a
BrianCamunez <124356373+BrianCamunez@users.noreply.github.com>
2024-10-03 18:51:00 +0000

    ejercio4

---
+++ Practicas/pac4-cuarta-app/array.php
@@ -0,0 +1,90 @@
+<?php
+$peliculas = [
+    [
+        'id' => 1, 
+        'nom' => 'Inception',
+        'imatge' => 'https://m.media-amazon.com/images/I/912AErFSBHL._AC_UF894,1000_QL80_.jpg',
+        'horaris' => ['14:00', '17:00', '20:00'],
+        'sinopsi' => 'Un expert en robar secrets del món dels somnis intenta fer un impossible: plantar una idea en la ment d\'una persona.',
+        'durada' => '148 min',
+        'director' => 'Christopher Nolan',
+        'repartiment' => ['Leonardo DiCaprio', 'Joseph Gordon-Levitt', 'Elliot Page'],
+        'qualificacio' => '+13',
+        'genere' => 'Acció, Sci-Fi',
+        'trailer' => 'https://www.youtube.com/watch?v=8hP9D6kZseM',
+        'valoracion' => 3,
+        'carusel' => ['https://m.media-amazon.com/images/M/MV5BMTQ1ZmIzOTAtNDcwZi00NDVkLWE4NWItYWNhZGY1MmVlZGU0XkEyXkFqcGdeQWRvb2xpbmhk._V1_.jpg','https://m.media-amazon.com/images/S/pv-target-images/e826ebbcc692b4d19059d24125cf23699067ab621c979612fd0ca11ab42a65cb._SX1080_FMjpg_.jpg','https://img.europapress.es/fotoweb/fotonoticia_20150606095824_690.jpg']
+    ],
+    [
+      'id' => 2, 
+        'nom' => 'The Matrix',
+        'imatge' => 'https://m.media-amazon.com/images/I/613ypTLZHsL._AC_UF894,1000_QL80_.jpg',
+        'horaris' => ['15:00', '18:00', '21:00'],
+        'sinopsi' => 'Un hacker descobreix la veritat darrere de la realitat que coneix, i es veu immers en una lluita contra les màquines que dominen el món.',
+        'durada' => '136 min',
+        'director' => 'Lana Wachowski, Lilly Wachowski',
+        'repartiment' => ['Keanu Reeves', 'Laurence Fishburne', 'Carrie-Anne Moss'],
+        'qualificacio' => '+13',
+        'genere' => 'Acció, Sci-Fi',
+        'trailer' => 'https://www.youtube.com/watch?v=vKQiBpx8J0s',
+        'valoracion' => 2
+    ],
+    [
+      'id' => 3, 
+        'nom' => 'The Shawshank Redemption',
+        'imatge' => 'https://m.media-amazon.com/images/I/51NiGlapXlL._AC_.jpg',
+        'horaris' => ['16:00', '19:00', '22:00'],
+        'sinopsi' => 'Un banquer és condemnat a cadena perpètua i forma una amistat amb un altre pres, buscant l’esperança en un sistema corruptor.',
+        'durada' => '142 min',
+        'director' => 'Frank Darabont',
+        'repartiment' => ['Tim Robbins', 'Morgan Freeman', 'Bob Gunton'],
+        'qualificacio' => '+16',
+        'genere' => 'Drama',
+        'trailer' => 'https://www.youtube.com/watch?v=6hB3S9bIaco',
+        'valoracion' => 5
+    ],
+    [
+      'id' => 4, 
+        'nom' => 'The Godfather',
+        'imatge' => 'https://m.media-amazon.com/images/M/MV5BYTJkNGQyZDgtZDQ0NC00MDM0LWEzZWQtYzUzZDEwMDljZWNjXkEyXkFqcGc@._V1_.jpg',
+        'horaris' => ['13:00', '16:00', '19:00'],
+        'sinopsi' => 'La història d\'una família mafiosa i les seves lluites internes per mantenir el poder.',
+        'durada' => '175 min',
+        'director' => 'Francis Ford Coppola',
+        'repartiment' => ['Marlon Brando', 'Al Pacino', 'James Caan'],
+        'qualificacio' => '+16',
+        'genere' => 'Drama, Criminal',
+        'trailer' => 'https://www.youtube.com/watch?v=sY1S34973zA',
+        'valoracion' => 1
+    ],
+    [
+      'id' => 5, 
+        'nom' => 'Pulp Fiction',
+        'imatge' => 'https://static.wikia.nocookie.net/wiki-doblaje-espana/images/c/cb/Pulpfictionposter.jpg/revision/latest?cb=20200229115619&path-prefix=es',
+        'horaris' => ['20:00', '23:00'],
+        'sinopsi' => 'Una narrativa no lineal que segueix diverses històries interconnectades de criminalitat a Los Angeles.',
+        'durada' => '154 min',
+        'director' => 'Quentin Tarantino',
+        'repartiment' => ['John Travolta', 'Uma Thurman', 'Samuel L. Jackson'],
+        'qualificacio' => '+18',
+        'genere' => 'Criminal, Drama',
+        'trailer' => 'https://www.youtube.com/watch?v=wZ8F9tqH81U',
+        'valoracion' => 4
+    ],
+    [
+      'id' => 6, 
+        'nom' => 'The Dark Knight',
+        'imatge' => 'https://m.media-amazon.com/images/S/pv-target-images/e9a43e647b2ca70e75a3c0af046c4dfdcd712380889779cbdc2c57d94ab63902.jpg',
+        'horaris' => ['18:00', '21:00'],
+        'sinopsi' => 'Batman s’enfronta al Joker, un criminal que posa a prova la moralitat de la ciutat de Gotham.',
+        'durada' => '152 min',
+        'director' => 'Christopher Nolan',
+        'repartiment' => ['Christian Bale', 'Heath Ledger', 'Aaron Eckhart'],
+        'qualificacio' => '+13',
+        'genere' => 'Acció, Thriller',
+        'trailer' => 'https://www.youtube.com/watch?v=EXeTwQWrcwY',
+        'valoracion' => 2
+    ]
+];
+
+?>
---
+++ Practicas/pac4-cuarta-app/detall.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detalls de la Pel·lícula</title>
+    <link rel="stylesheet">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    
+    <style>
+        body {
+    font-family: Arial, sans-serif;
+    background-color: #f4f4f4;
+    margin: 0;
+    padding: 0;
+}
+
+.container {
+    max-width: 800px;
+    margin: 20px auto;
+}
+
+.pelicula {
+    display: flex;
+    background-color: white;
+    border-radius: 8px;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+    overflow: hidden;
+}
+
+.imatge {
+    max-width: 300px; /* Ajusta el tamany de la imatge */
+    height: auto;
+}
+
+.detalls {
+    padding: 15px;
+    flex: 1; /* Ocupa l'espai restant */
+}
+
+h2 {
+    font-size: 1.8em;
+    margin: 10px 0;
+}
+
+.descripcio {
+    color: #555;
+    margin: 10px 0;
+}
+
+strong {
+    color: #333;
+}
+    </style>
+</head>
+<body>
+    <?php
+
+    include 'array.php';
+
+    $informacion = 0;
+
+    if(isset($_GET['id'])&& is_numeric($_GET['id'])){
+        $informacion = $peliculas['id'];
+        $informacion = ($_GET['id']);
+        foreach($peliculas as $pelicula){
+            if($informacion == $pelicula['id']){
+                $infoPeli = $pelicula;
+            }
+        }
+    }
+
+
+
+    ?>
+    <div class="container">
+        <div class="pelicula">
+            <?php
+            echo "<img src='{$infoPeli['imatge']}' alt='Inception' class='imatge'>";
+            echo '<div class="detalls">';
+            echo "<h2>{$infoPeli['nom']}</h2>";
+            echo  "<p class='descripcio'>{$infoPeli['sinopsi']}</p>";
+            echo  '<p>Durada:'.$infoPeli["durada"].'</p>';
+            echo "<p><strong>Director:</strong>{$infoPeli['director']}</p>";
+            echo "<p><strong>Repartiment:</strong>";
+            echo implode(" ", $infoPeli['repartiment']);
+            echo "</p>";
+            echo "<p><strong>Qualificació:</strong>{$infoPeli['qualificacio']}</p>";
+            echo "<p><strong>Gènere:</strong>{$infoPeli['genere']}</p>";
+            echo "<p><strong>Horaris:</strong>";
+            echo implode(" ", $infoPeli['horaris']);
+            echo "</p>";
+            switch($infoPeli['valoracion']){
+                case 1:
+                    echo "<span style='color: #E5BE01;'>&#9733</span>";  
+                    break;
+                case 2:
+                    echo "<span style='color: #E5BE01;'>&#9733</span>";  
+                    echo "<span style='color: #E5BE01;'>&#9733</span>"; 
+                    break; 
+                case 3:
+                    echo "<span style='color: #E5BE01;'>&#9733</span>";  
+                    echo "<span style='color: #E5BE01;'>&#9733</span>";  
+                    echo "<span style='color: #E5BE01;'>&#9733</span>"; 
+                    break;
+                case 4:
+                    echo "<span style='color: #E5BE01;'>&#9733</span>";  
+                    echo "<span style='color: #E5BE01;'>&#9733</span>";  
+                    echo "<span style='color: #E5BE01;'>&#9733</span>";  
+                    echo "<span style='color: #E5BE01;'>&#9733</span>";  
+                    break;
+                case 5:
+                    echo "<span style='color: #E5BE01;'>&#9733</span>";  
+                    echo "<span style='color: #E5BE01;'>&#9733</span>";  
+                    echo "<span style='color: #E5BE01;'>&#9733</span>";  
+                    echo "<span style='color: #E5BE01;'>&#9733</span>";  
+                    echo "<span style='color: #E5BE01;'>&#9733</span>";  
+                    break;
+            };
+            ?>
+            <div id="carouselExample" class="carousel slide">
+                <div class="carousel-inner">
+                <div class="carousel-item active">
+            <?php
+            $numero = 0;
+            ?>
+            <img src="<?php echo $infoPeli['carusel'][$numero]; ?>" class="d-block w-100" width="200" height="200" alt="Foto1">
+        </div>
+        <div class="carousel-item">
+            <img src="<?php echo $infoPeli['carusel'][$numero]; ?>" class="d-block w-100" width="200" height="200" alt="Foto2">
+        </div>
+        <div class="carousel-item">
+            <img src="<?php echo $infoPeli['carusel'][$numero]; ?>" class="d-block w-100" width="200" height="200" alt="Foto3">
+        </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                <?php
+                    $numero ++;
+                    ?>
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                <?php
+                    $numero --;
+                    ?>
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
+
+
+                    <!-- echo "<div class='carousel-item active'>";
+                    echo "<img src='{$infoPeli['carusel'][0]}' class='d-block w-100' witdh='200px' height='100px' alt='Foto1'>";
+                    echo "</div>";
+                    echo "<div class='carousel-item'>";
+                    echo "<img src='{$infoPeli['carusel'][1]}' class='d-block w-100' witdh='200px' height='100px' alt=Foto2>";
+                    echo "</div>";
+                    echo "<div class='carousel-item'>";
+                    echo "<img src='{$infoPeli['carusel'][2]}' class='d-block w-100' witdh='200px' height='100px' alt=Foto3>"; -->
